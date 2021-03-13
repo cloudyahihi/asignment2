be91a3a90dc393cf45cea7b791ed9398f92b4ca3
cloudyahihi <77433615+cloudyahihi@users.noreply.github.com>
2021-03-13 13:47:20 +0700

    Update app.js

--- app.js
+++ app.js
@@ -73,7 +73,11 @@ app.post('/search',async (req,res)=>{
         find({productName: new RegExp(searchText,'i')}).toArray();
     var n = searchText.length; 
     var a = searchText.charAt(n-1);
-    if (a<0 || a>1000)
+    if (a>0 && a<1000)
+    {
+        
+    }
+    else
     {
         res.render('home',{model:results})
     }
